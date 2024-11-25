b7de57c30076489945a967e49fa0c279fda19ab2
Ena Heleneto <by15242952083@outlook.com>
2024-11-25 23:31:07 +0800

    feat: 更新项目结构和依赖
    - 更新了 .github/FUNDING.yml
    - 重命名 .github/workflows/ci.yml 为 .github/workflows/ci.yml.copy.txt
    - 更新了 .gitignore
    - 删除了 LICENSE
    - 更新了 README.md
    - 删除了 README.zh-CN.md
    - 删除了 netlify.toml

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <main
+  <!-- <main
     px-4 py-10
     text="center gray-700 dark:gray-200"
   >
@@ -8,5 +8,8 @@
     <div mx-auto mt-5 text-center text-sm opacity-50>
       [Default Layout]
     </div>
+  </main> -->
+  <main h-full w-full flex flex-col>
+    <RouterView />
   </main>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,56 +1,12 @@
-<script setup lang="ts">
-defineOptions({
-  name: 'IndexPage',
-})
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
-const { t } = useI18n()
-</script>
-
+<!--
+ * @Author: Ena Heleneto by15242952083@outlook.com
+ * @Date: 2024-11-25 23:28:18
+ * @LastEditors: Ena Heleneto by15242952083@outlook.com
+ * @LastEditTime: 2024-11-25 23:29:06
+ * @FilePath: \Ena-Todo\src\pages\index.vue
+ * @Description:
+ * Copyright (c) 2024 by Ena Heleneto & by15242952083@outlook.com, All Rights Reserved.
+-->
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        m-3 text-sm btn
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
-    </div>
-  </div>
+  <div>index</div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: home
-</route>
